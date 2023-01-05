b50c954ee8eca4116d255f0b72bd420099972d8a
Yongfeng LI <wliyongfeng@gmail.com>
2023-01-05 10:02:29 +0800

    Init commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import * as IPFS from 'ipfs-core'
+
+;(async () => {
+  const node = await IPFS.create()
+  const stream = node.cat('QmeXMHxaaWRAUPAFa9KAuDLmf4w3kSYyWwjw6aDjtKncnW')
+  const decoder = new TextDecoder()
+  let data = ''
+
+  for await (const chunk of stream) {
+    // chunks of data are returned as a Uint8Array, convert it back to a string
+    data += decoder.decode(chunk, { stream: true })
+  }
+
+  console.log('data', data);
+})()
