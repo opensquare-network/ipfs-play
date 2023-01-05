6d887befa4c0115707fd7620deda152c17fd1b24
Yongfeng LI <wliyongfeng@gmail.com>
2023-01-05 16:35:22 +0800

    Update

--- src/axios.js
+++ src/axios.js
@@ -4,15 +4,15 @@ import { fileTypeFromBuffer } from 'file-type';
 const cids = [
   "QmaTVEBZtj6m579TuxTHc5dFDWkZXMb4rSDNXw4qVDYjYs",
   "QmYK58GeYcGMbjjfPDo69jsUU4A8PpLEyPcxBY1yJCSnD1",
-]
+];
 
-(async () => {
+;(async () => {
   for (const cid of cids) {
     const data = (await axios({
       url: `https://ipfs.io/ipfs/${ cid }`,
       responseType: "arraybuffer",
     })).data;
-    
+
     console.log(await fileTypeFromBuffer(data));
   }
 })();
