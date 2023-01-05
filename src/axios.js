28138a435e3d5177f66f511f766c57c1580f33db
Yongfeng LI <wliyongfeng@gmail.com>
2023-01-05 16:21:09 +0800

    Update

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { fileTypeFromBuffer } from 'file-type';
 
 (async () => {
-  const data = (await axios.get({
+  const data = (await axios({
     url: `https://ipfs.io/ipfs/QmaTVEBZtj6m579TuxTHc5dFDWkZXMb4rSDNXw4qVDYjYs`,
     responseType: "arraybuffer",
   })).data;
