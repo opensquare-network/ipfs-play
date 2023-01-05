c1e7b56a9b6061470d2b486119accaafaa3d3553
Yongfeng LI <wliyongfeng@gmail.com>
2023-01-05 16:33:13 +0800

    Update

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,18 @@
 import axios from "axios";
 import { fileTypeFromBuffer } from 'file-type';
 
+const cids = [
+  "QmaTVEBZtj6m579TuxTHc5dFDWkZXMb4rSDNXw4qVDYjYs",
+  "QmYK58GeYcGMbjjfPDo69jsUU4A8PpLEyPcxBY1yJCSnD1",
+]
+
 (async () => {
-  const data = (await axios({
-    url: `https://ipfs.io/ipfs/QmaTVEBZtj6m579TuxTHc5dFDWkZXMb4rSDNXw4qVDYjYs`,
-    responseType: "arraybuffer",
-  })).data;
-  console.log(await fileTypeFromBuffer(data));
+  for (const cid of cids) {
+    const data = (await axios({
+      url: `https://ipfs.io/ipfs/${ cid }`,
+      responseType: "arraybuffer",
+    })).data;
+    
+    console.log(await fileTypeFromBuffer(data));
+  }
 })();
