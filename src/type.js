378d153763e8c0b60df0b3a0bded3f17f09020df
Yongfeng LI <wliyongfeng@gmail.com>
2023-01-16 17:04:21 +0800

    Update

--- src/type.js
+++ src/type.js
@@ -1,6 +1,6 @@
 import { fileTypeFromFile, fileTypeFromStream } from 'file-type';
 import fs from 'node:fs';
 
-const stream = fs.createReadStream('./QmaTVEBZtj6m579TuxTHc5dFDWkZXMb4rSDNXw4qVDYjYs.jpeg');
+const stream = fs.createReadStream('./QmYK58GeYcGMbjjfPDo69jsUU4A8PpLEyPcxBY1yJCSnD1.mp4');
 console.log(await fileTypeFromStream(stream));
 // console.log(await fileTypeFromFile('./QmaTVEBZtj6m579TuxTHc5dFDWkZXMb4rSDNXw4qVDYjYs.jpeg'));
